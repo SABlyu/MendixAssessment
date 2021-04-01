49a623b4885f2f6d4686a11f4d843cbc0ffb2789
Brian Jones <sablyu@live.com>
2021-04-01 14:25:43 +0500

    dm-view: domains viewer hardcoded data

--- DomainsViewer/ViewModels/DomainsViewerViewModel.cs
+++ DomainsViewer/ViewModels/DomainsViewerViewModel.cs
@@ -1,13 +1,27 @@
-﻿using Prism.Mvvm;
+﻿using DomainModelEditor;
+using Domains.Common.Helpers;
+using Domains.Common.Models.Bindable;
+using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace Domains.Viewer.ViewModels
 {
     public class DomainsViewerViewModel : BindableBase, INavigationAware
     {
+        public DomainsViewerViewModel()
+        {
+
+        }
+
+
+        public ObservableCollection<BindableEntity> Domains { get; } = new ObservableCollection<BindableEntity>();
+
+
         public bool IsNavigationTarget(NavigationContext navigationContext) => true;
 
         public void OnNavigatedFrom(NavigationContext navigationContext)
@@ -16,6 +30,17 @@ namespace Domains.Viewer.ViewModels
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+            var entityStore = new EntityStore();
+            var demoData = new[]
+            {
+                new Tuple<int, string, int, int>(1, "Order", 100, 100),
+                new Tuple<int, string, int, int>(2, "OrderLine", 200, 200)
+            };
+            entityStore.Load(demoData);
+
+            entityStore
+                .Select(d => new BindableEntity(d))
+                .ForEach(bd => Domains.Add(bd));
         }
     }
 }
