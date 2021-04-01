0a0a48b1ca8021c308de68cec0fa6707d437ed19
Brian Jones <sablyu@live.com>
2021-04-01 14:24:47 +0500

    common: collection helper

---
+++ Common/Helpers/CollectionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Common.Helpers
+{
+    public static class CollectionHelper
+    {
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+                action(item);
+        }
+    }
+}
