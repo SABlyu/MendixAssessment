6bd5ad60b793569253d58350716e73d5f70d3390
Brian Jones <sablyu@live.com>
2021-04-02 16:28:00 +0500

    common: domains service

---
+++ Domains.Common/Services/DomainsService.cs
@@ -0,0 +1,29 @@
+﻿using Domains.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains.Common.Services
+{
+    public class DomainsService : DbItemService<Entity>
+    {
+        public DomainsService(AppDbContext db) : base(db)
+        {
+        }
+
+
+        public async Task DeleteItem(int id)
+        {
+            var item = await _db.Domains.FirstOrDefaultAsync(i => i.Id == id);
+            _db.Domains.Remove(item);
+        }
+
+        protected override IQueryable<Entity> IncludeProperties(IQueryable<Entity> query)
+        {
+            return query; // nothing to attach for now
+        }
+    }
+}
