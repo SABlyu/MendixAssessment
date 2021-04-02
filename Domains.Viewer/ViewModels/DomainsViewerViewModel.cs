81317562ce9d1e7ebd93892ebbfbb3ae2f6ee42a
Brian Jones <sablyu@live.com>
2021-04-02 14:15:53 +0500

    dm-viewer: renamed folder

--- Domains.Viewer/ViewModels/DomainsViewerViewModel.cs
+++ Domains.Viewer/ViewModels/DomainsViewerViewModel.cs
@@ -4,10 +4,8 @@ using Domains.Common.Models.Bindable;
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
 
 namespace Domains.Viewer.ViewModels
 {
---
+++ Domains.Viewer/Views/DomainsViewer.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Domains.Viewer.Views
+{
+    /// <summary>
+    /// Interaction logic for DomainsViewer.xaml
+    /// </summary>
+    public partial class DomainsViewer : UserControl
+    {
+        public DomainsViewer()
+        {
+            InitializeComponent();
+        }
+    }
+}
