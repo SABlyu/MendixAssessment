3e3882052a8197eeaddfc05e87fe63a976cef196
Brian Jones <sablyu@live.com>
2021-04-02 14:15:09 +0500

    common: renamed folder

---
+++ Domains.Common/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domains.Common.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Common
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Entity> Domains { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=domains.sqlite");
+            //optionsBuilder.UseLazyLoadingProxies();
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
