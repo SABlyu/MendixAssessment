83dc2fe2e406976b27f9dcd3b886435109e82ac1
Brian Jones <sablyu@live.com>
2021-04-03 15:07:52 +0500

    ui-tools: added UI tools library

---
+++ Domains.InterfaceTools/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Domains.InterfaceTools.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (bool)value ? Visibility.Visible : (Visibility)parameter;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (Visibility)value == Visibility.Visible;
+        }
+    }
+}
