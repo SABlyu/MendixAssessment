182874bebd6a65faeb87891ca3717a04b0fa0d78
Brian Jones <sablyu@live.com>
2021-04-03 13:12:04 +0500

    dm-viewer: domain editor dialog (basic stuff)

---
+++ Domains.Viewer/ViewModels/DomainEditorDialogViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Domains.Common.Models;
+using Domains.Common.Models.Bindable;
+using Domains.Common.Services;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Viewer.ViewModels
+{
+    public class DomainEditorDialogViewModel : BusyViewModel, IDialogAware
+    {
+        public DomainEditorDialogViewModel(DomainsService domainsService)
+        {
+
+        }
+
+        public string Title
+        {
+            get => _title;
+            private set => SetProperty(ref _title, value);
+        }
+
+        public BindableEntity Entity
+        {
+            get => _entity;
+            set => SetProperty(ref _entity, value);
+        }
+
+
+        public event Action<IDialogResult> RequestClose;
+
+        public bool CanCloseDialog() => !IsBusy;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            if (parameters.ContainsKey("entity"))
+            {
+                Title = "Edit entity";
+                Entity = parameters.GetValue<BindableEntity>("entity");
+            }
+            else
+            {
+                Title = "Create new entity";
+                Entity = new BindableEntity(new Entity());
+            }
+        }
+
+
+        private string _title;
+        private BindableEntity _entity;
+    }
+}
