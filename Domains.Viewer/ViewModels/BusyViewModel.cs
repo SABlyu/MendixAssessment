8da00b8b52f107009fecf84048521ff0a95a9780
Brian Jones <sablyu@live.com>
2021-04-03 13:11:23 +0500

    dm-viewer: busy view-model

---
+++ Domains.Viewer/ViewModels/BusyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domains.Viewer.ViewModels
+{
+    public class BusyViewModel : BindableBase
+    {
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+
+        protected bool _isBusy;
+    }
+}
