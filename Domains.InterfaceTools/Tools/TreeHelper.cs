74d60fc99f62ae198613efe9c6a06a5b198b9dce
Brian Jones <sablyu@live.com>
2021-04-05 11:52:13 +0500

    ui-tools: tree helper to get parent by type

---
+++ Domains.InterfaceTools/Tools/TreeHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace Domains.InterfaceTools.Tools
+{
+    public static class TreeHelper
+    {
+        public static T FindAncestor<T>(this DependencyObject dObj) where T : DependencyObject
+        {
+            var uiElement = dObj;
+            while (uiElement != null)
+            {
+                uiElement = VisualTreeHelper.GetParent(uiElement as Visual ?? new UIElement())
+                    //?? VisualTreeHelper.GetParent(uiElement as Visual3D ?? new UIElement())
+                    ?? LogicalTreeHelper.GetParent(uiElement);
+
+                if (uiElement is T) return (T)uiElement;
+            }
+            return null;
+        }
+    }
+}
