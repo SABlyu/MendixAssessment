a453c5839ae07650c0be8b850fc1666b669bb086
Brian Jones <sablyu@live.com>
2021-04-05 14:30:23 +0500

    common: multiple ways to create the app db context

--- Domains.Common/AppDbContext.cs
+++ Domains.Common/AppDbContext.cs
@@ -8,16 +8,44 @@ namespace Domains.Common
 {
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// connect to a database using default connection string
+        /// </summary>
+        public AppDbContext()
+        {
+            _connectionString = "Data Source=domains.sqlite";
+        }
+
+        /// <summary>
+        /// create a DB context using a custom connection string
+        /// </summary>
+        /// <param name="connectionString">connectoin string (say, from app config)</param>
+        public AppDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        /// create a DB context with custom options
+        /// </summary>
+        /// <param name="options">options</param>
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Entity> Domains { get; set; }
         public DbSet<DomainProperty> DomainProperties { get; set; }
 
 
-        protected override void OnConfiguring(
-            DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=domains.sqlite");
+            if (!string.IsNullOrEmpty(_connectionString))
+                optionsBuilder.UseSqlite(_connectionString);
+            
             //optionsBuilder.EnableSensitiveDataLogging(true);
+            
             base.OnConfiguring(optionsBuilder);
         }
+
+
+        private readonly string _connectionString;
     }
 }
